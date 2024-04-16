bd18707f032166e9c1abb04f123ac22ecc9cdc33
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-04-16 09:16:20 +0200

    Ändra menyn så att den är i en while loop

--- src/App.java
+++ src/App.java
@@ -3,35 +3,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+        }
 
     static void meny(){
 
         Scanner tangentbord = new Scanner(System.in);
-
-        int val = tangentbord.nextInt();
-        System.out.println("Hej! \n\nVälkommen till buss bokningssystem \n\n1. Boka \n2. Lediga platser \n3. Vinst \n4. Avsluta \n\nAnge ditt val: ");
         
-        if(val==1){
+        while (true) {
+            System.out.println("Hej! \n\nVälkommen till buss bokningssystem \n\n1. Boka \n2. Lediga platser \n3. Vinst \n4. Avsluta \n\nAnge ditt val: ");
+            int val = tangentbord.nextInt();
+
+            if(val==1){
+
+            }
+            else if(val==2){
+                
+            }
+            else if(val==3){
+
+            }
+            else if(val==4){
             
-        }
-        else if(val==2){
+            }
+            else{
+            System.out.println("Obs ange ditt val mellan 1-4");
+            }
 
-        }
-        else if(val==3){
-
-        }
-        else if(val==4){
-
-        }
-        else{
-            System.out.println("Ange ditt val mellan 1-4");
-        }
-
-        try{
+            //checka
+            try{
             val = tangentbord.nextInt();
-        } catch (Exception e) {
-            System.out.println("Ange ditt val i siffror");
+            } catch (Exception e) {
+            System.out.println("Obs ange ditt val i siffror");
+            }
         }
     }
 }
