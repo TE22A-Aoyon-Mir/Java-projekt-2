aec73b4ca819e0855f5a9aca6867c543d0fceca5
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2024-04-25 11:13:17 +0200

    Har lagt till en global array värde och gjort en metod för bokning i en while loop som inehåller födelsedatum, pris, betalning i while loop, for loop för array och en return.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-
+    static int[] plats = new int[20];
     public static void main(String[] args) throws Exception {
         start();
     }
@@ -25,7 +25,7 @@ public class App {
 
             switch (val) {
                 case 1:
-                    
+                    boka();
                     break;
                 case 2:
                     
@@ -39,6 +39,56 @@ public class App {
                     System.exit(0);
                 default:
                     System.out.println("\nOBS! Ange ditt val mellan 1-4.\n");
+            }
+        }
+    }
+
+    static void boka() {
+        Scanner tangentbord = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nAnge ditt födelsedatum i ååååmmdd: ");
+            int födelsedatum = 0;
+
+            try {
+                födelsedatum = tangentbord.nextInt();
+            } catch (Exception e) {
+                System.out.println("\nOBS! Ange ditt födelsedatum i siffror.");
+                tangentbord.nextLine();
+                continue;
+            }
+
+            double pris = 299.90;
+            System.out.println("\nBokning för en plats kostar " + pris + " kr. Ange betalningen: ");
+            
+            while (true) {
+                double betalning = 0;
+
+                try {
+                    betalning = tangentbord.nextDouble();
+                } catch (Exception e) {
+                    System.out.println("OBS! Ange betalningen i siffror.");
+                    tangentbord.nextLine();
+                    continue;
+                }
+            
+                if (betalning < pris) {
+                    System.out.println("Ogiltig betalning. Du måste betala minst " + pris + " kr.");
+                    continue;
+                } else {
+                    System.out.println("\nBetalning godkänd.");
+                    break;
+                }
+            }
+
+            System.out.println("\nHär är din plats: ");
+
+            for (int i = 0; i < plats.length; i++) {
+                if (plats[i] == 0) {
+                    System.out.println("\nPlats " + i + " bokad för dig med födelsedatum " + födelsedatum + "\n");
+                    plats[i] = födelsedatum;
+                    return;
+                }
             }
         }
     }
